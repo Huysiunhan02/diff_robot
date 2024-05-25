ef97ea3abfc295fb5117dfe2aac65065f5bb065c
Huysiunhan02 <buiquanghuy2kx@gmail.com>
2024-05-25 23:50:45 +0700

    edit launch_robot.py

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -8,7 +8,7 @@ from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessStart
+from launch.event_handlers import OnProcessExit
 
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
@@ -36,60 +36,51 @@ def generate_launch_description():
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[{'robot_description': ParameterValue(robot_description, value_type=str)},
-                    controller_params_file]
+                    controller_params_file],
+        output="both",
     )
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    # delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_cont"],
+        arguments=["diff_cont", "--controller-manager", "/controller_manager"],
     )
 
-    delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[diff_drive_spawner],
-        )
-    )
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessStart(
+    #         target_action=controller_manager,
+    #         on_start=[diff_drive_spawner],
+    #     )
+    # )
 
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_broad"],
+        arguments=["joint_broad",  "--controller-manager", "/controller_manager"],
     )
 
-    delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[joint_broad_spawner],
-        )
-    )
-
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
+    # delayed_joint_broad_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessStart(
+    #         target_action=controller_manager,
+    #         on_start=[joint_broad_spawner],
     #     )
     # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
-
+     # Delay start of diff_drive_controller_spawner after `joint_state_broadcaster`
+    delay_diff_drive_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_broad_spawner,
+            on_exit=[diff_drive_spawner],
+        )
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
-        delayed_controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
+        delay_diff_drive_controller_spawner_after_joint_state_broadcaster_spawner
+        # delayed_controller_manager,
+        # delayed_diff_drive_spawner,
+        # delayed_joint_broad_spawner
     ])
