c64784f9e3eca8afa8b7dbf73870e638e1aad216
Quang_Huy <96275325+Huysiunhan02@users.noreply.github.com>
2024-05-26 01:38:40 +0700

    fix launch_robot ok

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -8,18 +8,20 @@ from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
+from launch.event_handlers import OnProcessStart
+from launch_ros.parameter_descriptions import ParameterValue
 
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
+
 
 
 def generate_launch_description():
 
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='diff_robot'
+    package_name='diff_robot' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -36,8 +38,7 @@ def generate_launch_description():
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[{'robot_description': ParameterValue(robot_description, value_type=str)},
-                    controller_params_file],
-        output="both",
+                    controller_params_file]
     )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
@@ -45,42 +46,52 @@ def generate_launch_description():
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_cont", "--controller-manager", "/controller_manager"],
+        arguments=["diff_cont","--controller-manager", "/controller_manager"],
     )
 
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessStart(
-    #         target_action=controller_manager,
-    #         on_start=[diff_drive_spawner],
-    #     )
-    # )
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
 
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_broad",  "--controller-manager", "/controller_manager"],
+        arguments=["joint_broad","--controller-manager", "/controller_manager"],
     )
 
-    # delayed_joint_broad_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessStart(
-    #         target_action=controller_manager,
-    #         on_start=[joint_broad_spawner],
-    #     )
-    # )
-
-     # Delay start of diff_drive_controller_spawner after `joint_state_broadcaster`
-    delay_diff_drive_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_broad_spawner,
-            on_exit=[diff_drive_spawner],
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
         )
     )
+
+
+    # Code for delaying a node (I haven't tested how effective it is)
+    # 
+    # First add the below lines to imports
+    # from launch.actions import RegisterEventHandler
+    # from launch.event_handlers import OnProcessExit
+    #
+    # Then add the following below the current diff_drive_spawner
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[diff_drive_spawner],
+    #     )
+    # )
+    #
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+
+
 
     # Launch them all!
     return LaunchDescription([
         rsp,
-        delay_diff_drive_controller_spawner_after_joint_state_broadcaster_spawner,
         delayed_controller_manager,
-        # delayed_diff_drive_spawner,
-        # delayed_joint_broad_spawner
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
     ])
